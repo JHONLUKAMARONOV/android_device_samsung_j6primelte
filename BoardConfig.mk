90d2b8e661f9b9447435c2e061a373ee6a7f22ee
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2019-12-31 19:13:57 +0100

    j6primelte: code cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,56 +14,53 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := device/samsung/j6primelte
+
 # For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES :=true
+ALLOW_MISSING_DEPENDENCIES := true
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
 
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
+
+# Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := SRPPI22A000RU
 
-# Platform
-TARGET_BOARD_PLATFORM := msm8917
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
-
-#Architecture
-TARGET_ARCH := arm
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-
+# Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS :=  --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --second_offset 0x00f00000 --tags_offset 0x01e00000
 BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_PREBUILT_KERNEL := device/samsung/j6primelte/zImage-dtb
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/zImage-dtb
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-
-# Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4 
 
-# TWRP specific build flags
-TW_DEVICE_VERSION := SebaUbuntu
+# TWRP Configuration
 TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_EXCLUDE_SUPERSU := true
-TW_MAX_BRIGHTNESS := 255
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_CRYPTO_USE_SYSTEM_VOLD := true
-TW_IGNORE_MISC_WIPE_DATA := true
-TW_CRYPTO_USE_SYSTEM_VOLD := true
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_NTFS_3G := true
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_IGNORE_MISC_WIPE_DATA := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_HAS_DOWNLOAD_MODE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
--- omni_j6primelte.mk
+++ omni_j6primelte.mk
@@ -1,5 +1,5 @@
-# Release name
-PRODUCT_RELEASE_NAME := j6primelte
+#
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,20 @@ PRODUCT_RELEASE_NAME := j6primelte
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit OF stuff
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j6primelte
